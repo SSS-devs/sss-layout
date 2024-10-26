005394eb9dd9b4a7a9001354b7319fb701b56521
Rahammetoela <fuseteam@duck.com>
2024-10-26 09:42:41 -0300

    turn the openstreetmap link into a link with a location linked

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -5,9 +5,9 @@ var map = L.map('map', {
 		zoomControl: {{ site.data.sitetext[site.locale].contact.map.zoom.control | default: false }},
 		dragging: {{ site.data.sitetext[site.locale].contact.map.zoom.drag | default: false }},
 		doubleClickZoom: {{ site.data.sitetext[site.locale].contact.map.zoom.doubleclick | default: false }},
-		scrollWheelZoom: false } ).setView([ {{ site.data.sitetext[site.locale].contact.map.location.latitude }}, {{ site.data.sitetext[site.locale].contact.map.location.longitude }} ], {{ site.data.sitetext[site.locale].contact.map.zoom.level | default: 17 }} );
+		scrollWheelZoom: false } ).setView([ {{ site.data.sitetext[site.locale].contact.map.center.latitude }}, {{ site.data.sitetext[site.locale].contact.map.center.longitude }} ], {{ site.data.sitetext[site.locale].contact.map.zoom.level | default: 17 }} );
 L.tileLayer('{{ site.data.sitetext[site.locale].contact.map.server }}', {
     maxZoom: {{ site.data.sitetext[site.locale].contact.map.zoom.max | default: 19 }},
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/?mlat={{ site.data.sitetext[site.locale].contact.map.location.latitude }}&mlon={{ site.data.sitetext[site.locale].contact.map.location.longitude }}#map={{ site.data.sitetext[site.locale].contact.map.zoom.level }}/{{ site.data.sitetext[site.locale].contact.map.center.latitude }}/{{ site.data.sitetext[site.locale].contact.map.center.longitude }}">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([ {{ site.data.sitetext[site.locale].contact.map.location.latitude }}, {{ site.data.sitetext[site.locale].contact.map.location.longitude }} ]).addTo(map);
